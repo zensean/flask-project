d2bbd8a48434aa11927d48870ae9cd99217366d2
Sean <zensean00@gmail.com>
2024-01-12 07:45:03 +0800

    static files

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app=Flask(__name__) # 建立 Application 物件
 #建立路徑 / 對應的處理函式
 @app.route("/")
 def index(): # 用來回應路徑 / 的處理函式
-    return "Hello Flask" #回傳網站首頁<body>中的內容
+    return "Hello Flask" #回傳路徑 / <body>中的內容
 
 #建立路徑 /data 對應的處理函式
 @app.route("/data")
